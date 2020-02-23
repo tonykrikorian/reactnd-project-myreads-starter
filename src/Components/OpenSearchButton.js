b3c01e719bbb61189509f66fc310b49a7e6fb3dc
Tony <tony.krikorian@outlook.com>
2020-02-23 16:18:05 -0300

    Add Link to searchPage

--- src/Components/OpenSearchButton.js
+++ src/Components/OpenSearchButton.js
@@ -1,9 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const OpenSearchButton = () => {
   return (
     <div className="open-search">
-      <button onClick={() => alert("Hello")}>Add a book</button>
+      <Link to="/search">
+        <button>Add a book</button>
+      </Link>
     </div>
   );
 };
