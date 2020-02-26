54e491b4d71ac948ac0506e6ee118b4d130221a4
Tony <tkrikoriam@achs.cl>
2020-02-26 09:19:42 -0300

    Fix for invalid query and undefined thumbnail

--- src/Components/Bookshelf.js
+++ src/Components/Bookshelf.js
@@ -8,45 +8,52 @@ const Bookshelf = ({
   handleOnChangeBookShelf,
   shelfToMove
 }) => {
-  Bookshelf.PropTypes = {
-    dataBooks: PropTypes.array.isRequired,
-    handleOnChangeBookShelf: PropTypes.func.isRequired,
-    shelfToMove: PropTypes.string.isRequired
-  };
   return (
     <div className="bookshelf">
       <h2 className="bookshelf-title">{title}</h2>
       <div className="bookshelf-books">
         <ol className="books-grid">
-          {dataBooks.map(book => (
-            <li key={book.id}>
-              <div className="book">
-                <div className="book-top">
-                  <div
-                    className="book-cover"
-                    style={{
-                      width: 128,
-                      height: 193,
-                      backgroundImage: `url(${book.imageLinks.thumbnail})`
-                    }}
-                  ></div>
-                  <BookShelfChanger
-                    handleOnChangeBookShelf={handleOnChangeBookShelf}
-                    bookId={book.id}
-                    book={book}
-                    shelfToMove={shelfToMove}
-                    actualShelf={book.shelf ? book.shelf : "none"}
-                  />
-                </div>
-                <div className="book-title">{book.title}</div>
-                <div className="book-authors">{book.authors}</div>
-              </div>
-            </li>
-          ))}
+          {"error" in dataBooks ? (
+            <span>There's no results</span>
+          ) : (
+            <React.Fragment>
+              {dataBooks.map(book => (
+                <li key={book.id}>
+                  <div className="book">
+                    <div className="book-top">
+                      <div
+                        className="book-cover"
+                        style={{
+                          width: 128,
+                          height: 193,
+                          backgroundImage: `url(${
+                            !book.imageLinks ? "" : book.imageLinks.thumbnail
+                          })`
+                        }}
+                      ></div>
+                      <BookShelfChanger
+                        handleOnChangeBookShelf={handleOnChangeBookShelf}
+                        bookId={book.id}
+                        book={book}
+                        shelfToMove={shelfToMove}
+                        actualShelf={book.shelf ? book.shelf : "none"}
+                      />
+                    </div>
+                    <div className="book-title">{book.title}</div>
+                    <div className="book-authors">{book.authors}</div>
+                  </div>
+                </li>
+              ))}
+            </React.Fragment>
+          )}
         </ol>
       </div>
     </div>
   );
 };
-
+Bookshelf.PropTypes = {
+  dataBooks: PropTypes.array.isRequired,
+  handleOnChangeBookShelf: PropTypes.func.isRequired,
+  shelfToMove: PropTypes.string.isRequired
+};
 export default Bookshelf;
